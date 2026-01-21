2573e8b2c782b6e7fc8228194b0e615f3abe1df5
harumorit <2323027@asia-u.ac.jp>
2026-01-21 13:57:19 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// ↓↓↓ ここを Firebase コンソールの値に置き換える
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBZ9xXjz9mxQhyHJSvCAv1U5Yqvd_GEU9M",
+  authDomain: "bio2-quiz.firebaseapp.com",
+  projectId: "bio2-quiz",
+  storageBucket: "bio2-quiz.appspot.com",
+  messagingSenderId: "918787492585",
+  appId: "1:918787492585:web:54d6f6024dc006cb0bfe78"
 };
 
 const app = initializeApp(firebaseConfig);
