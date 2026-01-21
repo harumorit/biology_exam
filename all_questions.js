7f9194cfcba47ff966f26fe11a6cf1123b10044b
harumorit <2323027@asia-u.ac.jp>
2026-01-21 13:21:31 +0900

    Add files via upload

---
+++ all_questions.js
@@ -0,0 +1,18 @@
+import { Q01 } from "./questions/q01.js";
+import { Q02 } from "./questions/q02.js";
+import { Q03 } from "./questions/q03.js";
+import { Q04 } from "./questions/q04.js";
+import { Q05 } from "./questions/q05.js";
+import { Q06 } from "./questions/q06.js";
+import { Q07 } from "./questions/q07.js";
+import { Q08 } from "./questions/q08.js";
+import { Q09 } from "./questions/q09.js";
+import { Q10 } from "./questions/q10.js";
+import { Q11 } from "./questions/q11.js";
+import { Q12 } from "./questions/q12.js";
+import { Q13 } from "./questions/q13.js";
+
+export const QUESTIONS = [
+  ...Q01, ...Q02, ...Q03, ...Q04, ...Q05, ...Q06, ...Q07,
+  ...Q08, ...Q09, ...Q10, ...Q11, ...Q12, ...Q13
+];
